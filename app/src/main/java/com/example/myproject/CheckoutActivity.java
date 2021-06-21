69cfed66282b171ed132f51a63ba7269e437d4c1
Marc Matias <mrcallenmatias@gmail.com>
2021-06-20 22:54:49 -0700

    commit

--- app/src/main/java/com/example/myproject/CheckoutActivity.java
+++ app/src/main/java/com/example/myproject/CheckoutActivity.java
@@ -2,7 +2,9 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +12,97 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        initHomeButton();
+        initMenuButton();
+        initBagButton();
+        initAboutButton();
+    }
+    private void initHomeButton(){
+
+        View icon = findViewById(R.id.homeIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CheckoutActivity.this, MainActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+
+    private void initMenuButton(){
+
+        View icon = findViewById(R.id.menuIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CheckoutActivity.this, MenuActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+
+            }
+
+        });
+
+    }
+
+    private void initAboutButton(){
+
+        View icon = findViewById(R.id.aboutIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CheckoutActivity.this, AboutActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    private void initBagButton(){
+
+        View icon = findViewById(R.id.bagIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CheckoutActivity.this, BagActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/myproject/InformationActivity.java
@@ -0,0 +1,41 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.7),(int)(height*.7));
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y=-20;
+
+        getWindow().setAttributes(params);
+        reviewOrder();
+    }
+
+    public void reviewOrder(){
+        Button reviewButton = findViewById(R.id.reviewButton);
+        reviewButton.setOnClickListener(v -> {
+            Intent intent = new Intent(InformationActivity.this, CheckoutActivity.class);
+            startActivity(intent);
+        });
+    }
+}
