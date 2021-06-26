87480bed0c1230d2fcd1d427a9e9d7c01169204b
finalkyos <daddyorca18@gmail.com>
2021-06-25 23:21:50 -0700

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/example/myproject/orderComplete.java
@@ -0,0 +1,28 @@
+package com.example.myproject;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+//coded by: Atilla Varga
+
+public class orderComplete extends AppCompatActivity {
+
+
+    TextView ordernum = findViewById(R.id.orderDisplay);
+    TextView date = findViewById(R.id.dateDisplay);
+    TextView yourord = findViewById(R.id.yourOrdDisplay);
+    TextView total = findViewById(R.id.totalDisplay);
+    TextView name = findViewById(R.id.nameDisplay);
+    TextView type = findViewById(R.id.typeDisplay);
+    TextView email = findViewById(R.id.emailDisplay);
+    TextView stat = findViewById(R.id.statDisplay);
+    TextView add = findViewById(R.id.pickAddress);
+
+}
