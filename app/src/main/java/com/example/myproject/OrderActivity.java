5c9ff9f63d9d541172604118dd3abe97e4d7a55b
Marc Matias <mrcallenmatias@gmail.com>
2021-06-25 23:48:17 -0700

    commit

--- app/src/main/java/com/example/myproject/OrderActivity.java
+++ app/src/main/java/com/example/myproject/OrderActivity.java
@@ -2,7 +2,9 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,5 +12,95 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        initAboutButton();
+        initBagButton();
+        initHomeButton();
+        initMenuButton();
+    }
+
+    private void initHomeButton(){
+
+        View icon = findViewById(R.id.homeIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OrderActivity.this, MainActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+
+    private void initMenuButton(){
+
+        View icon = findViewById(R.id.menuIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OrderActivity.this, MenuActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+
+            }
+
+        });
+
+    }
+
+    private void initAboutButton(){
+
+        View icon = findViewById(R.id.aboutIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OrderActivity.this, AboutActivity.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+    private void initBagButton(){
+
+        View icon = findViewById(R.id.bagIcon);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(OrderActivity.this, Bag_Activity.class);
+
+                startActivity(intent);
+
+            }
+
+        });
     }
 }
